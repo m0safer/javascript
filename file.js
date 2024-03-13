7e52b34fdc4db0f5c035eef0820310a7a88720d7
mosafer <foladi169@gmail.com>
2024-03-13 22:59:58 +0330

    massage  for me

---
+++ file.js
@@ -0,0 +1,51 @@
+// //console.log("conected...");
+// //document.write("conected...");
+
+
+// // var x ,y ,z;
+
+// //x = 12;
+// // y = 8;
+// // z = x + y;
+// //  document.write(z);
+// //  console.log(z);
+// //  window.alert(z);
+
+// //----------------------------------------------------------
+
+// // var Number= 23 + 22 +"codeyad"
+// var string = 20 + "   " + "codeyad"
+
+
+
+
+// var number = 16;
+// var number2 = 16;
+// var isMain;
+// isMain = (number == number2);
+// document.write(isMain);
+
+
+// var person = {
+//     Fname: "ali",
+//     Lname: "folad"
+// };
+
+
+// document.write(person.Fname);
+// document.write(person.Fname + person.Lname );
+
+// ------------------------------------------------------------
+
+// arraye
+
+// var colors = ["red" , "green" , "blue" , "black"];
+
+//     console.log(colors);
+
+
+// var Names = new Array("ali", "mamad", "reza");
+
+// console.log(Names[1]);
+var One = new Array(12, 13, 14);
+console.log(One);
